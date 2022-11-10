b4378755a3bc9ca4198d72b42744b77adf9a41b7
Zoltan Kovacs <zoltan@ikovacs.com>
2022-11-09 21:24:55 -0600

    changes

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.micro"
