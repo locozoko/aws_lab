fa11f86a89014fac40279dd5e3ebeb4d4cf98d9d
Zoltan Kovacs <zoltan@ikovacs.com>
2022-11-09 23:17:13 -0600

    changes

--- outputs.tf
+++ outputs.tf
@@ -12,25 +12,6 @@ ${join("\n", distinct(module.cc_vm.availability_zone))}
 All CC Instance IDs:
 ${join("\n", module.cc_vm.id)}
 
-All CC Management IPs:
-${join("\n", module.cc_vm.private_ip)}
-
-All CC Service IPs:
-Service Interface Device Index 1:
-${join("\n", module.cc_vm.cc_service_private_ip)} 
-
-Service Interface Device Index 2:
-${join("\n", module.cc_vm.cc_med_lrg_service_1_private_ip)} 
-
-Service Interface Device Index 3:
-${join("\n", module.cc_vm.cc_med_lrg_service_2_private_ip)} 
-
-Service Interface Device Index 4:
-${join("\n", module.cc_vm.cc_lrg_service_3_private_ip)}
-
-All CC Primary Service ENIs:
-${join("\n", module.cc_vm.service_eni_1)}
-
 All CC IAM Role ARNs (Please provide this to Zscaler for callhome enablement):
 ${join("\n", module.cc_iam.iam_instance_profile_arn)}
 
