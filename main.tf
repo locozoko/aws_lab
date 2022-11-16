a56a806922c6231788056aa4da3f93eaf9392207
Zoltan Kovacs <zoltan@ikovacs.com>
2022-11-16 09:48:04 -0600

    added a second cloud connector

---
+++ main.tf
@@ -0,0 +1,169 @@
+################################################################################
+# Generate a unique random string for resource name assignment and key pair
+################################################################################
+resource "random_string" "suffix" {
+  length  = 8
+  upper   = false
+  special = false
+}
+
+
+################################################################################
+# Map default tags with values to be assigned to all tagged resources
+################################################################################
+locals {
+  global_tags = {
+    Owner                                                                                 = var.owner_tag
+    ManagedBy                                                                             = "terraform"
+    Vendor                                                                                = "Zscaler"
+    "zs-edge-connector-cluster/${var.name_prefix}-cluster-${random_string.suffix.result}" = "shared"
+  }
+}
+
+
+################################################################################
+# The following lines generates a new SSH key pair and stores the PEM file 
+# locally. The public key output is used as the instance_key passed variable 
+# to the ec2 modules for admin_ssh_key public_key authentication.
+# This is not recommended for production deployments. Please consider modifying 
+# to pass your own custom public key file located in a secure location.   
+################################################################################
+resource "tls_private_key" "key" {
+  algorithm = var.tls_key_algorithm
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "${var.name_prefix}-key-${random_string.suffix.result}"
+  public_key = tls_private_key.key.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.key.private_key_pem
+  filename        = "../${var.name_prefix}-key-${random_string.suffix.result}.pem"
+  file_permission = "0600"
+}
+
+
+################################################################################
+# 1. Create/reference all network infrastructure resource dependencies for all 
+#    child modules (vpc, igw, nat gateway, subnets, route tables)
+################################################################################
+module "network" {
+  source            = "github.com/locozoko/zscc_tf_lab/modules/terraform-zscc-network-aws"
+  name_prefix       = var.name_prefix
+  resource_tag      = random_string.suffix.result
+  global_tags       = local.global_tags
+  zpa_enabled       = var.zpa_enabled
+  workloads_enabled = var.workloads_enabled
+  az_count          = var.az_count
+  vpc_cidr          = var.vpc_cidr
+  cc_subnets        = var.cc_subnets
+  route53_subnets   = var.route53_subnets
+  #bring-your-own variables
+  byo_vpc        = var.byo_vpc
+  byo_vpc_id     = var.byo_vpc_id
+  byo_subnets    = var.byo_subnets
+  byo_subnet_ids = var.byo_subnet_ids
+}
+
+
+################################################################################
+# 2. Create specified number CC VMs per cc_count which will span equally across 
+#    designated availability zones per az_count. E.g. cc_count set to 4 and 
+#    az_count set to 2 will create 2x CCs in AZ1 and 2x CCs in AZ2
+################################################################################
+# Create the user_data file with necessary bootstrap variables for Cloud Connector registration
+locals {
+  userdata = <<USERDATA
+[ZSCALER]
+CC_URL=${var.cc_vm_prov_url}
+SECRET_NAME=${var.secret_name}
+HTTP_PROBE_PORT=${var.http_probe_port}
+USERDATA
+}
+
+# Write the file to local filesystem for storage/reference
+resource "local_file" "user_data_file" {
+  content  = local.userdata
+  filename = "../user_data"
+}
+
+# Create specified number of CC appliances
+module "cc_vm" {
+  source                    = "github.com/locozoko/zscc_tf_lab/modules/terraform-zscc-ccvm-aws"
+  cc_count                  = var.cc_count
+  name_prefix               = var.name_prefix
+  resource_tag              = random_string.suffix.result
+  global_tags               = local.global_tags
+  mgmt_subnet_id            = module.network.cc_subnet_ids
+  service_subnet_id         = module.network.cc_subnet_ids
+  instance_key              = aws_key_pair.deployer.key_name
+  user_data                 = local.userdata
+  ccvm_instance_type        = var.ccvm_instance_type
+  cc_instance_size          = var.cc_instance_size
+  iam_instance_profile      = module.cc_iam.iam_instance_profile_id
+  mgmt_security_group_id    = module.cc_sg.mgmt_security_group_id
+  service_security_group_id = module.cc_sg.service_security_group_id
+
+  depends_on = [
+    local_file.user_data_file,
+    null_resource.cc_error_checker,
+  ]
+}
+
+################################################################################
+# 3. Create IAM Policy, Roles, and Instance Profiles to be assigned to CC. 
+#    Default behavior will create 1 of each IAM resource per CC VM. Set variable 
+#    "reuse_iam" to true if you would like a single IAM profile created and 
+#    assigned to ALL Cloud Connectors instead.
+################################################################################
+module "cc_iam" {
+  source              = "github.com/locozoko/zscc_tf_lab/modules/terraform-zscc-iam-aws"
+  iam_count           = var.reuse_iam == false ? var.cc_count : 1
+  name_prefix         = var.name_prefix
+  resource_tag        = random_string.suffix.result
+  global_tags         = local.global_tags
+  cc_callhome_enabled = var.cc_callhome_enabled
+
+  byo_iam = var.byo_iam
+  # optional inputs. only required if byo_iam set to true
+  byo_iam_instance_profile_id = var.byo_iam_instance_profile_id
+  # optional inputs. only required if byo_iam set to true
+}
+
+
+################################################################################
+# 4. Create Security Group and rules to be assigned to CC mgmt and and service 
+#    interface(s). Default behavior will create 1 of each SG resource per CC VM. 
+#    Set variable "reuse_security_group" to true if you would like a single 
+#    security group created and assigned to ALL Cloud Connectors instead.
+################################################################################
+module "cc_sg" {
+  source       = "github.com/locozoko/zscc_tf_lab/modules/terraform-zscc-sg-aws"
+  sg_count     = var.reuse_security_group == false ? var.cc_count : 1
+  name_prefix  = var.name_prefix
+  resource_tag = random_string.suffix.result
+  global_tags  = local.global_tags
+  vpc_id       = module.network.vpc_id
+
+  byo_security_group = var.byo_security_group
+  # optional inputs. only required if byo_security_group set to true
+  byo_mgmt_security_group_id    = var.byo_mgmt_security_group_id
+  byo_service_security_group_id = var.byo_service_security_group_id
+  # optional inputs. only required if byo_security_group set to true
+}
+
+################################################################################
+# Validation for Cloud Connector instance size and EC2 Instance Type 
+# compatibilty. Terraform does not have a good/native way to raise an error at 
+# the moment, so this will trigger off an invalid count value if there is an 
+# improper deployment configuration.
+################################################################################
+resource "null_resource" "cc_error_checker" {
+  count = local.valid_cc_create ? 0 : "Cloud Connector parameters were invalid. No appliances were created. Please check the documentation and cc_instance_size / ccvm_instance_type values that were chosen" # 0 means no error is thrown, else throw error
+  provisioner "local-exec" {
+    command = <<EOF
+      echo "Cloud Connector parameters were invalid. No appliances were created. Please check the documentation and cc_instance_size / ccvm_instance_type values that were chosen" >> ../errorlog.txt
+EOF
+  }
+}
