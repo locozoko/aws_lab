51d759aaa75b297ae049740cc77c17e5ceba5cf4
Zoltan Kovacs <zoltan@ikovacs.com>
2023-04-17 15:37:38 -0500

    added zpa

--- modules/terraform-zsac-acvm-aws/versions.tf
+++ modules/terraform-zsac-acvm-aws/versions.tf
@@ -2,11 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.7.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.3.0"
+      version = "~> 4.58.0"
     }
     local = {
       source  = "hashicorp/local"
@@ -16,19 +12,6 @@ terraform {
       source  = "hashicorp/null"
       version = "~> 3.1.0"
     }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 3.4.0"
-    }
   }
-
   required_version = ">= 0.13.7, < 2.0.0"
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = var.aws_region
-}
-
-provider "zpa" {
 }
